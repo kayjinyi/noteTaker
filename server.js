1cb652fb1406aa6b90307f0729b86c6316a06c44
kayjinyi <95940771+kayjinyi@users.noreply.github.com>
2022-04-27 02:15:58 -0700

    add delete

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
+const uuid = require("./helpers/uuid");
+const notes = require("./db/db.json");
+const fs = require("fs");
 
 //serves css/js/static assets
 app.use(express.static("public"));
@@ -11,43 +14,63 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
+  res.json(`${req.method} request received to get reviews`);
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
     if (err) {
       throw err;
     } else {
-      const note = JSON.parse(data);
-      res.json(note);
+      const notes = JSON.parse(data);
+      console.log(notes);
     }
   });
 });
 
 app.post("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", "utf-8", (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      const people = JSON.parse(data);
-      console.log(req.body);
-      people.push(req.body);
-      fs.writeFile(
-        "./db/db.json",
-        JSON.stringify(people, null, 2),
-        (err, data) => {
-          if (err) {
-            throw err;
-          } else {
-            res.json(people);
-          }
-        }
-      );
-    }
-  });
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+  const titleIn = req.body.title;
+  const textIn = req.body.text;
+  if (titleIn && textIn) {
+    const newNote = {
+      titleIn,
+      textIn,
+      id: uuid(),
+    };
+
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        const notes = JSON.parse(data);
+        console.log(req.body);
+        notes.push(newNote);
+        const notesString = JSON.stringify(notes);
+        fs.writeFile(
+          "./db/db.json",
+          notesString,
+          (err, data) => (err) =>
+            err
+              ? console.error(err)
+              : console.log(`Note has been written to JSON file`)
+        );
+      }
+    });
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting notes");
+  }
 });
 
-// app.delete("/api/notes/:id", (req, res) => {
-//   console.log(req.method, req.url);
-//   res.send("delete request to /animals");
-// });
+app.delete("/api/notes/:id", (req, res) => {
+  console.log(req.method, req.url);
+  res.send("delete request to /notes");
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
